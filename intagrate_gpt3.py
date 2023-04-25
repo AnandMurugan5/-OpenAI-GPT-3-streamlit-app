0dc52f257976142607710dbdcf4b953df8a9f87a
Muruganandham R <muruganandham.r@ctlep.local>
2023-04-25 16:08:07 +0530

    issues fixed

--- intagrate_gpt3.py
+++ intagrate_gpt3.py
@@ -12,7 +12,7 @@ from langchain.llms.openai import OpenAI
 from langchain.agents import AgentExecutor
 
 
-os.environ['OPENAI_API_KEY'] = "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"
+os.environ['OPENAI_API_KEY'] = "org-CVfNMe4AFK4VbHuZIaJndtZK"
 
 #connected sqlite database
 db = SQLDatabase.from_uri("sqlite:///./swbe_data.db")
